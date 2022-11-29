137fc6eae7a436b76d694023e92c9a25f6670c01
Markmutuku <mark.mutuku@student.moringaschool.com>
2022-11-29 12:13:01 +0300

    Yet to Complete Wrestling Web page

---
+++ app.js
@@ -0,0 +1,19 @@
+function ageOfMinors(age) {
+    if (age < 18) {
+       console.log('Don\'t drink.'); 
+    } else if (age < 21) {
+       console.log('Dont\'t work.'); 
+    } else (age > 21); {
+        console.log('You can do as you please.');
+    }
+}
+ageOfMinors (19);
+ageOfMinors (25);
+ageOfMinors (16)
+
+function kidsNames(names) {
+    for (let nam =nam < array.lengnam++) {
+        const element = arnam];
+        
+    }
+}
